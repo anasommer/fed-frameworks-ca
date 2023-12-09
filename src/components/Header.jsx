a927c49fe163612038e922967bf8e74812bf6614
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-12-09 19:44:12 +0100

    app structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,24 @@
 import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
+  const activeStyles = {
+    fontWeight: 'bold',
+    textDecoration: 'underline',
+    color: '#161616',
+  };
+
   return (
     <header>
       <Link className='site-logo' to='/'>
         eCom Store
       </Link>
       <nav>
-        <NavLink>Home</NavLink>
-        <NavLink>Cart</NavLink>
-        <NavLink>Contact</NavLink>
+        <NavLink to='/'>Home</NavLink>
+
+        <NavLink to='/contact'>Contact</NavLink>
+        <NavLink to='/cart'>
+          <img src='/src/assets/images/cart.svg' alt='' className='cart-icon' />
+        </NavLink>
       </nav>
     </header>
   );
