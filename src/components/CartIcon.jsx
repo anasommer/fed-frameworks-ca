72f3115b736cf7d8fcbaddd1cd776941c1536125
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-12-10 11:50:58 +0100

    add cart items count overlay

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -1,20 +1,29 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function CartIcon() {
   const activeStyles = {
     filter:
       'invert(8%) sepia(82%) saturate(6462%) hue-rotate(1deg) brightness(99%) contrast(84%)',
   };
+
+  const [cartItemsCount, setCartItemsCount] = useState(2);
+
   return (
-    <NavLink
-      to='/cart'
-      style={({ isActive }) => (isActive ? activeStyles : null)}
-    >
-      <img
-        src='/src/assets/images/cart.svg'
-        alt='shopping cart icon'
-        className='cart-icon'
-      />
-    </NavLink>
+    <>
+      <NavLink
+        to='/cart'
+        style={({ isActive }) => (isActive ? activeStyles : null)}
+      >
+        <img
+          src='/src/assets/images/cart.svg'
+          alt='shopping cart icon'
+          className='cart-icon'
+        />
+      </NavLink>
+      {cartItemsCount && (
+        <span className='items-counter'>{cartItemsCount}</span>
+      )}
+    </>
   );
 }
