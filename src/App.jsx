60238abedc447499bb7c9f6121e9a85ff70023af
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-12-10 12:11:13 +0100

    added basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import ContactPage from './pages/ContactPage';
+import Layout from './components/Layout';
+import CheckOutPage from './pages/CheckoutPage';
 
 function App() {
   return (
     <>
-      <Header />
+      <Layout />
+
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='cart' element={<CheckOutPage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,3 @@
+export default function CheckOutPage() {
+  return <h2>Checkout page</h2>;
+}
