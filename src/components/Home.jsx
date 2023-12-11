e425d8cdb24618053d248af1193ff22c3c37b870
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-12-11 20:30:30 +0100

    added reusable api function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,41 @@
+import { apiUrl } from '../constants/api';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
-  return <h1>HOMEPAGE</h1>;
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(null);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        setProducts(data);
+      } catch (error) {
+        setIsError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      {isLoading && <div>Loading products...</div>}
+      {isError && <div>{isError}</div>}
+      {products &&
+        products.map((product) => (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+            <p>{product.price}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
