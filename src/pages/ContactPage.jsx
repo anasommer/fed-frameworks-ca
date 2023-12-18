ca9ffb264fbf2024503203ce863125b5353ef785
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-12-18 17:59:36 +0100

    added basic html structure for contact form

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,3 +1,31 @@
 export default function ContactPage() {
-  return <h2>Contact page</h2>;
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className='form-container'>
+      <h1>Contact form</h1>
+      <form onSubmit={handleSubmit} className='contact-form'>
+        <label>
+          Full Name <br />
+          <input type='text' name='name' placeholder='Full Name' />
+        </label>
+        <label>
+          Subject <br />
+          <input type='text' name='subject' placeholder='Subject' />
+        </label>
+        <label>
+          Email <br />
+          <input type='email' name='email' placeholder='E-mail' />
+        </label>
+        <label>
+          Message <br />
+          <textarea placeholder='Message' />
+        </label>
+
+        <button>Send</button>
+      </form>
+    </div>
+  );
 }
