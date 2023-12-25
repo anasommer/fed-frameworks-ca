f6121f6499492080e7c9dca2358bf2b06b8bb36e
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-12-25 17:56:58 +0100

    added total price to the cart

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -4,6 +4,12 @@ import useStore from '../store/cart';
 export default function CheckOutPage() {
   const { cart, removeFromCart, clearCart } = useStore();
 
+  const price = cart.map((product) => product.price);
+
+  const total = price
+    .reduce((acc, currentValue) => acc + currentValue, 0)
+    .toFixed(2);
+
   return (
     <div>
       <h2>Checkout page</h2>
@@ -18,6 +24,7 @@ export default function CheckOutPage() {
           ))}
           {cart.length > 0 && <button onClick={clearCart}>Clear Cart</button>}
         </ul>
+        <span>Total: {total}$</span>
       </div>
       <Link to={`/checkoutSuccess`}>
         <button className='link-button' onClick={clearCart}>
